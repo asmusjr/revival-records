ff965762083ca11a19d0529ddae3c0e6ebc9e930
alibraay <alibraay@uwec.edu>
2014-11-13 09:44:51 -0600

    music player

--- js/main.js
+++ js/main.js
@@ -1,8 +1,1 @@
-$(function() {
-	var li = $('#top-nav a[href$="/' + location.pathname.split("/")[2] + '"]');
-	if(li.length == 0) {
-		$('#top-nav a[href$="index.php"]').parent().addClass('active');
-	} else {
-		li.parent().addClass('active');
-	}
-})
+
