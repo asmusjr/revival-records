9cdcd2847ca059a50a1de2cb8f4b6571a6b8ac68
asmusjr <asmusjr@uwec.edu>
2014-11-13 11:53:01 -0600

    Adding music player to header.php
    Other pages need js scripts in header; will add soon

--- header-footer.php
+++ header-footer.php
@@ -1,39 +1,81 @@
-<div class="navbar navbar-inverse navbar-static-top navbar-custom" role="navigation">
-	<div class="container">
+	<div class="navbar navbar-inverse navbar-static-top navbar-custom" role="navigation">
+	  <div class="container">
 		<div class="navbar-header">
-			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-				<span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#"><img id="navbar-image" src="img/logo.png" alt="Revival Records"></a>
+		  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+			<span class="sr-only">Toggle navigation</span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		  </button>
+		  <a class="navbar-brand" href="#"><img id="navbar-image" src="img/logo.png" alt="Revival Records"></a>
 		</div>
-		<div id="top-nav" class="navbar-collapse collapse">
-			<ul class="nav navbar-nav navbar-right">
-				<li><a href="/test/index.php">Home</a></li>
-                <li><a href="/test/about.php">About</a></li>
-                <li><a href="/test/contact.php">Contact</a></li>
-                <li class="dropdown">
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Records <span class="caret"></span></a>
-					<ul class="dropdown-menu" role="menu">
-						<li><a href="/test/records.php">About our Records</a></li>
-						<li><a href="/test/record_catalog.php">View all Records</a></li>
-						<li class="divider"></li>
-						<li><a href="/test/wishlist.php">Wishlist</a></li>
-						<li><a href="/test/request.php">Send Request</a></li>
-					</ul>
-                </li>
-			</ul>
-		</div>
-	</div>
-</div>
 		
-<nav class="navbar navbar-fixed-bottom" role="navigation">
-	<footer>
-		<div class="container">
-			<p class="pull-right"><a href="/test/visit.php">128 S. Barstow St &middot; Eau Claire</a></p>
-			<p><a href="/test/visit.php">715-514-4202</a></p>
-		</div><!-- /.container -->
-	</footer>
-</nav>
+		<div id="top-nav" class="navbar-collapse collapse">
+		  <ul class="nav navbar-nav navbar-right">
+			<li><a href="index.php">Home</a></li>
+			<li><a href="about.php">About the Store</a></li>
+			<li><a href="contact.php">Contact</a></li>
+			<li class="dropdown">
+			  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Records <span class="caret"></span></a>
+			  <ul class="dropdown-menu" role="menu">
+				<li><a href="records.php">About our Records</a></li>
+				<li><a href="record_catalog.php">View all Records</a></li>
+				<li class="divider"></li>
+				<li><a href="wishlist.php">Wishlist</a></li>
+				<li><a href="request.php">Send Request</a></li>
+			  </ul>
+			</li>
+			<li><div id="jquery_jplayer_1" class="jp-jplayer"></div>
+				<div id="jp_container_1" class="jp-audio">
+					<div class="jp-type-single">
+						<div class="jp-gui jp-interface">
+							<ul class="jp-controls">
+								<li><a href="javascript:;" class="jp-play" tabindex="1">play</a></li>
+								<li><a href="javascript:;" class="jp-pause" tabindex="1">pause</a></li>
+								<li><a href="javascript:;" class="jp-stop" tabindex="1">stop</a></li>
+								<li><a href="javascript:;" class="jp-mute" tabindex="1" title="mute">mute</a></li>
+								<li><a href="javascript:;" class="jp-unmute" tabindex="1" title="unmute">unmute</a></li>
+								<li><a href="javascript:;" class="jp-volume-max" tabindex="1" title="max volume">max volume</a></li>
+								
+								
+							</ul>
+							
+							<div class="jp-progress" style= "transform: translate(0px, -20px)">
+								<div class="jp-seek-bar">
+									<div class="jp-play-bar"></div>
+								</div>
+							</div>
+							<div class="jp-volume-bar">
+								<div class="jp-volume-bar-value"></div>
+							 </div>
+							 
+							 <div class="jp-time-holder" style= "transform: translate(0px, -20px)">
+								<div class="jp-current-time"></div>
+								<div class="jp-duration"></div>
+								<ul class="jp-toggles">
+									<li><a href="javascript:;" class="jp-repeat" tabindex="1" title="repeat">repeat</a></li>
+									<li><a href="javascript:;" class="jp-repeat-off" tabindex="1" title="repeat off">repeat off</a></li>
+								</ul>
+							 </div>
+						</div>
+					
+					<div class="jp-no-solution">
+						<span>Update Required</span>
+							To play the media you will need to either update your browser to a recent version or update your <a href="http://get.adobe.com/flashplayer/" target="_blank">Flash plugin</a>.
+					</div>
+				</div>
+			</div></li>
+			
+		  </ul>
+		</div>
+	  </div>
+	</div>
+
+	<nav class="navbar navbar-fixed-bottom" role="navigation">
+		<footer>
+			<div class="container">
+				<p class="pull-right"><a href="visit.php">128 S. Barstow St &middot; Eau Claire</a></p>
+				<p><a href="#">715-514-4202</a></p>
+			</div><!-- /.container -->
+		</footer>
+	</nav>
--- index.php
+++ index.php
@@ -1,20 +1,48 @@
+
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<title>Revival Records - Eau Claire Records</title>
-		<meta name="description" content="">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
-		<link rel="stylesheet" href="css/master.css">
-		<script src="js/vendor/modernizr-2.6.2.min.js"></script>
-	</head>
-	<body>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Revival Records - Eau Claire Records</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+        <link rel="stylesheet" href="css/master.css">
+        
+		
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+        <script type="text/javascript" src="http://code.jquery.com/jquery-1.7.2.min.js"></script>
+    	<script type="text/javascript" src="js/jQuery.jPlayer.2.7.0/jquery.jplayer.min.js"></script>
+  <script type="text/javascript">
+    $(document).ready(function(){
+      $("#jquery_jplayer_1").jPlayer({
+        ready: function () {
+          $(this).jPlayer("setMedia", {
+            title: "Test song",
+            m4a: "http://www.jplayer.org/audio/m4a/Miaow-07-Bubble.m4a",
+            oga: "http://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
+          });
+        },
+        swfPath: "js/jQuery.jPlayer.2.7.0",
+        supplied: "m4a, oga"
+      });
+    });
+  </script>
+  
+    </head>
+    
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        
 		<?php include 'header-footer.php' ?>
+
 		<div class="container">
 			<div id="myCarousel" class="carousel slide" data-ride="carousel">
 			  <!-- Indicators -->
@@ -61,6 +89,7 @@
 		</div>
 
 		<div class="container main-content">
+		  <!-- Three columns of text below the carousel -->
 		  <div class="marketing row">
 			  <div class="col-lg-4">
 				<img class="img-circle" src="img/P1050121.jpg" alt="Generic placeholder image" style="width: 140px; height: 140px;">
@@ -78,14 +107,15 @@
 			<div class="col-lg-4">
 			  <img class="img-circle" src="img/P1050061.jpg" alt="Generic placeholder image" style="width: 140px; height: 140px;">
 			  <h2>WE'VE MOVED TO A BIGGER STORE!</h2>
-			  <p>Now Located at 128 S. Barstow St</p>
-			  <p>Grand Opening Wed, March 5, 2014 at 11:00 AM!</p>
+			  <p>Now Located at 128 S. Barstow St
+
+	Grand Opening Wed, March 5, 2014 at 11:00 AM!</p>
 			  <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
 			</div><!-- /.col-lg-4 -->
 		  </div><!-- /.row -->
 		</div>
 
-        <script src="js/vendor/jquery-1.10.2.min.js"></script>
+        
         <script src="js/bootstrap.min.js"></script>
         <script src="js/plugins.js"></script>
         <script src="js/main.js"></script>
