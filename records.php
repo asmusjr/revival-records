e48226b64f27b12f42f76411177d5ebfeefeedd3
calebolson100 <olsonct@uwec.edu>
2014-11-23 16:26:34 -0600

    Front end validation on form
    This now has some front end validation on the form for record request.
    As we spoke about on Thursday this is going to get moved to the page
    where we have the list of all our records (My Records?) where we can
    then submit the form via the modal. So this stuff I'm adding is going to
    get moved around, but the validation is there now.

--- records.php
+++ records.php
@@ -1,4 +1,34 @@
-
+    <?php
+		/*function postRecordRequest(){
+            echo "buttstick";
+			$servername = "68.178.217.19";
+			$username = "revivalrecordsdb";
+			$password = "RevivalRecords123!";
+			$dbname = "revivalrecordsdb";
+            
+            $conn = mysql_connect($servername, $username, $password); if (!$conn) {die("Connection failed: " . mysqli_connect_error());}
+            $db_selected = mysql_select_db($dbname, $conn);  if (!$db_selected) {die ('Can\'t use the db : ' . mysql_error());}
+            
+            $name = $_POST["name"];
+            $email = $_POST["email"];
+            $phone = $_POST["phone"];
+            $record = $_POST["record"];
+            $request_first = $_POST["firstName"];
+            $request_last = $_POST["lastName"];
+            $current_date = date("Y-m-d H:i:s");
+            $record_id = $_POST["recordID"];
+            // run a query to get records and populate dropdown with existing records to assign request_record in below query
+			$sql = mysql_query("INSERT INTO Record_Request (record_id, request_email, request_phone, request_record, request_first, request_last, request_date)
+			VALUES ('$record_id', '$email', '$phone', null, '$request_first', '$request_last', '$current_date')");
+			if ($sql) {
+				echo "New record created successfully";
+			} else {
+				echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+			}
+			mysqli_close($conn);
+			echo "DONE JEAH";
+		}*/
+	?>
   <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
@@ -53,9 +83,9 @@
 
 <body>
 		<?php include 'header-footer.php' ?>
-		<div class="container">
+		
 <h1 align = "right" style = "padding: 10px 350px">Records</h1>
-
+</br></br></br></br></br>
 <!--<div class="row">-->
 		<div id="thumbnail" class = "polaroid_imgs" style="margin-left:15%;">
 			<a href="img/revdif.png" title=""><img height="150" src="img/revdif.png" alt="img1" rel=""/></a>
@@ -104,6 +134,49 @@
 </br></br>
 
 
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+        <h4 class="modal-title" id="myModalLabel">Request a Record</h4>
+      </div>
+      <div class="modal-body">
+      
+      <form action="/webformmailer.php" method="post">
+      <input type="hidden" name="subject" value="Submission" />
+      <input type="hidden" name="redirect" value="records.php" />
+      <div class="form-group">
+    		<label for="inputFirstName">First Name</label>
+			<input type="text" name="First name" class="form-control" id="inputFirstName" placeholder="First Name" data-validation="length" data-validation-length="min1">
+	  </div>
+      <div class="form-group">
+        	<label for="inputFirstName">Last Name</label>
+			<input type="text" name="Last name" class="form-control" id="inputFirstName" placeholder="First Name" data-validation="length" data-validation-length="min1">
+	  </div>
+      <div class="form-group">
+			<label for="inputEmail">Email</label>
+			<input type="text" name="email" class="form-control" id="inputEmail" placeholder="Email" data-validation="email">
+	  </div>
+      <div class="form-group">
+			<label for="inputPhone">Phone</label>
+			<input type="text" name="Phone" class="form-control" id="inputPhone" placeholder="Phone" data-validation="number">
+	  </div>
+      <div class="form-group">
+			<label for="inputRecord">Record</label>
+			<input type="text" name="Record" class="form-control" id="inputRecord" placeholder="Record" data-validation="length" data-validation-length="min1">
+	  </div>
+      <div class="modal-footer">
+            <button type="button" class="btn btn-lg" data-dismiss="modal">Close</button>
+            <button type="submit" name="submit" class="btn btn-lg btn-primary">Submit Request</button>
+      </div>
+      <input type="hidden" name="form_order" value="alpha"/>
+      <input type="hidden" name="form_delivery" value="default"/> 
+      <input type="hidden" name="form_format" value="html"/>
+      </form>
+      
+
 		<!--<form action="records.php" method="post">
 			<div class="form-group">
 				<label for="inputFirstName">First Name</label>
@@ -139,8 +212,11 @@
     </div>
   </div>
 </div>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.1.47/jquery.form-validator.min.js"></script>
+        <script> $.validate(); </script>
 		<script src="js/bootstrap.min.js"></script>
         <script src="js/plugins.js"></script>
         <script src="js/main.js"></script>
-	</body>
+</body>
+
 </html> 
