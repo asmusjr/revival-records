75aa57397d0cef34471add5eccc1efe83cb8e990
asmusjr <asmusjr@uwec.edu>
2014-12-04 11:39:57 -0600

    record catalog rough draft

--- record_catalog.php
+++ record_catalog.php
@@ -17,70 +17,173 @@
     </head>
 	<body>
 		<?php include 'header-footer.php' ?>
-						
-		<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-  			<div class="modal-dialog">
-				<div class="modal-content">
-					<div class="modal-header">
-						<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-						<h4 class="modal-title" id="myModalLabel">Login</h4>
+		
+		<div class="container">
+			<div class="row">
+				<div class="main-content col-md-10">
+					<h2 class="content-large-header">Record <span class="text-muted">Catalog</span></h2>
+					<p>Welcome to the record catalog section of Revival Records. Here, you can view...</p>
+					<p>To learn about how we get our records, see about ...</p>
+					<p>To request records to pick up the store ... then go to ... </p>
+					
+					<hr/>
+					<ul class="nav nav-pills" role="tablist">
+					  <li role="presentation" class="active"><a href="#">Sort by Artist</a></li>
+					  <li role="presentation"><a href="#">Sort by Album</a></li>
+					</ul>
+				</div>
+				<div class="col-md-2 text-center">
+					<div class="row">
+						<div class="col-xs-12">
+							<p><a href="record_catalog.php"><img class="active-record-link" src="img/recordcatalogicon.jpg"/></a></p>
+						</div>
 					</div>
-					<div class="modal-body">
-						<form>
-							<div class="form-group">
-								<label for="inputEmail">Email/Username</label>
-								<input type="email" class="form-control" id="inputEmail" placeholder="Email/Username">
-							</div>
-							<div class="form-group">
-								<label for="inputRecord">Password</label>
-								<input type="record" class="form-control" id="inputRecord" placeholder="Password">
-							</div>
-						</form>
-					</div>
-					<div class="modal-footer">
-						<button type="button" class="btn btn-lg" data-dismiss="modal">Close</button>
-						<button type="button" class="btn btn-lg btn-primary">Login</button>
+					<div class="row">
+						<div class="col-xs-12">
+							<p><a href="my_records.php"><img src="img/requestrecord.jpg"/></a></p>
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-			
-		<div class="recordsTitle" align="center">
-			<h2 style="font-size: 36px; padding: 2px 25px">Record <span class="text-muted">Catalog</span></h2>
-		</div>
-		<div class = "catalog" align = "center">
-			<div class="pagination" >
-				<ul align = "center">
-					<li class="active"><a href="#">A</a></li>
-					<li><a href="#">B</a></li>
-					<li><a href="#">C</a></li>
-					<li><a href="#">D</a></li>
-					<li><a href="#">E</a></li>
-					<li><a href="#">F</a></li>
-					<li><a href="#">G</a></li>
-					<li><a href="#">H</a></li>
-					<li><a href="#">I</a></li>
-					<li><a href="#">J</a></li>
-					<li><a href="#">K</a></li>
-					<li><a href="#">L</a></li>
-					<li><a href="#">M</a></li>
-					<li><a href="#">N</a></li>
-					<li><a href="#">O</a></li>
-					<li><a href="#">P</a></li>
-					<li><a href="#">Q</a></li>
-					<li><a href="#">R</a></li>
-					<li><a href="#">S</a></li>
-					<li><a href="#">T</a></li>
-					<li><a href="#">U</a></li>
-					<li><a href="#">V</a></li>
-					<li><a href="#">W</a></li>
-					<li><a href="#">X</a></li>
-					<li><a href="#">Y</a></li>
-					<li><a href="#">Z</a></li>
-				</ul>
+			<div class="row">
+				<div class="col-xs-12 text-center">
+					<ul class="pagination">
+						<li class="active"><a href="#A">A</a></li>
+						<li><a href="#B">B</a></li>
+						<li><a href="#C">C</a></li>
+						<li><a href="#D">D</a></li>
+						<li><a href="#">E</a></li>
+						<li><a href="#">F</a></li>
+						<li><a href="#">G</a></li>
+						<li><a href="#">H</a></li>
+						<li><a href="#">I</a></li>
+						<li><a href="#">J</a></li>
+						<li><a href="#">K</a></li>
+						<li><a href="#">L</a></li>
+						<li><a href="#">M</a></li>
+						<li><a href="#">N</a></li>
+						<li><a href="#">O</a></li>
+						<li><a href="#">P</a></li>
+						<li><a href="#">Q</a></li>
+						<li><a href="#">R</a></li>
+						<li><a href="#">S</a></li>
+						<li><a href="#">T</a></li>
+						<li><a href="#">U</a></li>
+						<li><a href="#">V</a></li>
+						<li><a href="#">W</a></li>
+						<li><a href="#">X</a></li>
+						<li><a href="#">Y</a></li>
+						<li><a href="#">Z</a></li>
+					</ul>
+				</div>
 			</div>
+			<div class="row">
+			<div class="col-xs-12">
+			<div class="bs-docs-section">
+			<div class="bs-glyphicons">
+    <ul class="bs-glyphicons-list">
+      
+        <li id="A">
+		  
+		  <div class="thumbnail">
+      <img src="img/record.png" width="75px" alt="...">
+      <div class="caption">
+        <p>Label</p>
 		</div>
-			<div class = "container main-content">
+    </div>
+        <p><button class="btn btn-sm btn-primary"><span class="glyphicon glyphicon-plus"></span></button></p>
+		  
+        </li>
+      
+        <li>
+			<ul class="media-list">
+				<li class="media">
+					<a class="media-left" href="#">
+						<img src="img/record.png" width="50px" alt="...">
+						<a href="#"><span class="glyphicon glyphicon-plus"></span></a>
+					</a>
+				</li>
+				<p>Taylor Swift</p>
+				<p>1989</p>
+				<p>Pop</p>
+			</ul>
+		</li>
+      
+        <li>
+          <span class="glyphicon glyphicon-euro" aria-hidden="true"></span>
+          <span class="glyphicon-class">glyphicon glyphicon-euro</span>
+        </li>
+      
+        <li>
+          <span class="glyphicon glyphicon-minus" aria-hidden="true"></span>
+          <span class="glyphicon-class">glyphicon glyphicon-minus</span>
+        </li>
+      
+        <li id="B">
+          <span class="glyphicon glyphicon-cloud" aria-hidden="true"></span>
+          <span class="glyphicon-class">glyphicon glyphicon-cloud</span>
+        </li>
+      
+        <li>
+          <span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>
+          <span class="glyphicon-class">glyphicon glyphicon-envelope</span>
+        </li>
+      
+        <li>
+          <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+          <span class="glyphicon-class">glyphicon glyphicon-pencil</span>
+        </li>
+      
+        <li>
+          <span class="glyphicon glyphicon-glass" aria-hidden="true"></span>
+          <span class="glyphicon-class">glyphicon glyphicon-glass</span>
+        </li>
+      
+        <li id="C">
+          <span class="glyphicon glyphicon-music" aria-hidden="true"></span>
+          <span class="glyphicon-class">glyphicon glyphicon-music</span>
+        </li>
+      
+        <li>
+          <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+          <span class="glyphicon-class">glyphicon glyphicon-search</span>
+        </li>
+      
+        <li>
+          <span class="glyphicon glyphicon-heart" aria-hidden="true"></span>
+          <span class="glyphicon-class">glyphicon glyphicon-heart</span>
+        </li>
+      
+        <li>
+          <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+          <span class="glyphicon-class">glyphicon glyphicon-star</span>
+        </li>
+      
+        <li id="D">
+          <span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
+          <span class="glyphicon-class">glyphicon glyphicon-star-empty</span>
+        </li>
+      
+        <li>
+          <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+          <span class="glyphicon-class">glyphicon glyphicon-user</span>
+        </li>
+      
+        <li>
+          <span class="glyphicon glyphicon-film" aria-hidden="true"></span>
+          <span class="glyphicon-class">glyphicon glyphicon-film</span>
+        </li>
+      
+        <li>
+          <span class="glyphicon glyphicon-th-large" aria-hidden="true"></span>
+          <span class="glyphicon-class">glyphicon glyphicon-th-large</span>
+        </li>
+    </ul>
+  </div>
+			</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-12 main-content">
 				<table cellpadding="0" width="100%" cellspacing="0" style = "font-size: 16px">
 				<tr>
 					<td>
